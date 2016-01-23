a617b766084b16effffec6b90bc804951e928e51
multikatt <github@kattfest.se>
2016-01-23 08:14:13 -0500

    tor support

--- xmr.py
+++ xmr.py
@@ -9,10 +9,11 @@ import argparse
 
 class Xmr():
 
-    def __init__(self):
+    def __init__(self, tor=False):
         self.headers = {"Content-Type": "application/json"}
         self.xmrapi = "https://xmr.to/api/v1/xmr2btc/"
         self.walletapi = "http://localhost:18082/json_rpc"
+        self.tor = tor
 
     def usdtobtc(self, amount):
         tick = requests.get("https://poloniex.com/public?command=returnTicker")
@@ -25,17 +26,22 @@ class Xmr():
             amount = self.usdtobtc(amount)
         params = {"btc_amount": amount, "btc_dest_address": destination}
         api = self.xmrapi + "order_create/"
-        _r = self.post(params, api)
+        _r = self.post(params, api, local=False)
         return _r.json()["uuid"]
 
     def getstatus(self, uuid):
         params = {"uuid": uuid}
         api = self.xmrapi + "order_status_query/"
-        _r = self.post(params, api)
+        _r = self.post(params, api, local=False)
         return _r.json()
 
-    def post(self, params, api):
-        _r = requests.post(api, headers=self.headers,
+    def post(self, params, api, local=True):
+        if not local and self.tor:
+            print "using tor to connect to %s" % api
+            proxies = {"http": "socks5://localhost:9050",
+                       "https": "socks5://localhost:9050"}
+        _r = requests.post(api,
+                           headers=self.headers,
                            data=json.dumps(params))
         return _r
 
@@ -73,12 +79,14 @@ if __name__ == "__main__":
     parser.add_argument("amount")
     parser.add_argument("address")
     parser.add_argument("-u", "--usd", action='store_true')
+    parser.add_argument("-t", "--tor", action='store_true')
     args = parser.parse_args()
     amount = args.amount
     destination = args.address
     usd = args.usd
+    tor = args.tor
 
-    _t = Xmr()
+    _t = Xmr(tor=tor)
     uuid = _t.gentrans(amount, destination, usd)
     _r = _t.getstatus(uuid)
     while _r["state"] == "TO_BE_CREATED":
