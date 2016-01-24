98e620f4c36698dcd68ea5c29f2da5aefe9c9067
multikatt <github@kattfest.se>
2016-01-24 01:58:46 -0500

    simple setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(name="xmr",
+      description="Send btc via xmr.to",
+      author="multikatt",
+      url="https://github.com/multikatt/xmr",
+      scripts=["xmr.py"])
